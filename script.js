103081edb4d8979bae273cbb2971b152f07eddb6
slimMay <77777784+slimMay@users.noreply.github.com>
2025-04-23 16:08:08 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
-var swiper = new Swiper (".slide-content", {
-    slidesPerView: 4,
-    spaceBetween: 25,
-    loop: true,
-    centerSlide: true,
-    fade: true,
-    grabCursor: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+const data = [
+  { name: "小明", area: "高雄區", no: "1", img: "https://i.imgur.com/ZcLLrkY.jpg" },
+  { name: "小華", area: "台北區", no: "2", img: "https://i.imgur.com/qIufhof.jpg" }
+];
 
-    breakpoints: {
-        0: { slidesPerView: 1, },
-        520: { slidesPerView: 2 },
-        950: { slidesPerView: 4 },
+function showInfo() {
+  const wrapper = document.getElementById("cardWrapper");
+  data.forEach(card => {
+    const slide = document.createElement("div");
+    slide.className = "card swiper-slide";
+    slide.innerHTML = `
+      <img class="card-img" src="${card.img}" alt="">
+      <div class="card-content">
+        <h2 class="name">${card.name}</h2>
+        <p class="description">${card.area}　${card.no}</p>
+      </div>
+    `;
+    wrapper.appendChild(slide);
+  });
+
+  new Swiper(".swiper", {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
     },
-})
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    }
+  });
+}
