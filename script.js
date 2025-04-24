0d80229f1dabf9b48108b7d19ae6eb336f1e6d1e
slimMay <77777784+slimMay@users.noreply.github.com>
2025-04-24 17:04:20 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
-const data = [
-  { name: "小明", area: "高雄區", no: "1", img: "https://i.imgur.com/ZcLLrkY.jpg" },
-  { name: "小華", area: "台北區", no: "2", img: "https://i.imgur.com/qIufhof.jpg" }
-];
 
 function showInfo() {
-  const wrapper = document.getElementById("cardWrapper");
-  data.forEach(card => {
-    const slide = document.createElement("div");
-    slide.className = "card swiper-slide";
-    slide.innerHTML = `
-      <img class="card-img" src="${card.img}" alt="">
-      <div class="card-content">
-        <h2 class="name">${card.name}</h2>
-        <p class="description">${card.area}　${card.no}</p>
-      </div>
-    `;
-    wrapper.appendChild(slide);
-  });
+  fetch("https://script.google.com/macros/s/AKfycbz3MMlBffFJJm2NZrDZG-VrmpHbQtC-d9n0-Mc1jEzJ881DbT1Vk3lk0tBI24y8xK0Xhg/exec")
+    .then(res => res.json())
+    .then(data => {
+      const wrapper = document.getElementById("cardWrapper");
+      data.forEach(card => {
+        const slide = document.createElement("div");
+        slide.className = "card swiper-slide";
+        slide.innerHTML = `
+  <img class="card-img" src="${card.pictureLink}" alt="">
+  <div class="card-content">
+    <h2 class="name">${card.cardName}</h2>
+    <p class="description">${card.cardArea}　${card.cardNo}</p>
+  </div>
+`;
 
-  new Swiper(".swiper", {
-    slidesPerView: 2,
-    spaceBetween: 20,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev"
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true
-    }
-  });
+        wrapper.appendChild(slide);
+      });
+
+      new Swiper(".swiper", {
+        slidesPerView: 2,
+        spaceBetween: 20,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true
+        }
+      });
+    })
+    .catch(err => console.error("載入資料失敗：", err));
 }
